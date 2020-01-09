0c6809faef8fd12c4621379ee02380bb28d9d36a
andrey <andrey@debian.office.parallel.ua>
2020-01-09 15:28:35 +0200

    Init

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Directory
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.web_console.whitelisted_ips = '192.168.0.0/23'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
