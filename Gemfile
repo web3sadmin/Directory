f73392fde23bf6a19e1b5bf66961914ba96d7049
Andrey Viktorov <andrey.boyko86@gmail.com>
2024-11-01 15:02:43 +0000

    add small howto.txt

--- Gemfile
+++ Gemfile
@@ -8,9 +8,9 @@ end
 # Install manager dependencies
 gem 'bundler'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails'
+gem 'rails', '7.2.2'
 # Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.18', '< 0.6.0'
+gem 'mysql2', '>= 0.5.6', '< 0.6.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -37,9 +37,9 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'mini_portile2', '~> 2.8', '>= 2.8.7'
 
-gem 'rails'
-
 gem 'rubyzip', '2.4.rc1'
+
+gem 'bigdecimal'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Directory
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    #config.web_console.whitelisted_ips = '0.0.0.0/0'
-    config.web_console.whitelisted_ips = '127.0.0.1'
-    config.web_console.automount = false
+    config.web_console.whitelisted_ips = '0.0.0.0/0'
+    #config.web_console.whitelisted_ips = '127.0.0.1'
+    config.web_console.automount = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
